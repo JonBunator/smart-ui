726cb29148c07e697888a240e4595bf2437f3f14
JonBunator <62108893+JonBunator@users.noreply.github.com>
2025-05-27 13:57:11 +0200

    Remove types module

--- src/src/main.ts
+++ src/src/main.ts
@@ -7,3 +7,11 @@ export { SmartGroup } from './components/SmartGroup'
 export { SmartAgentProvider, useSmartAgent } from './components/SmartAgentProvider'
 export { SmartComponentManager, useSmartComponentManager } from './components/SmartComponentManager'
 export * from './utils/helpers'
+export type { SmartComponentValue } from './utils/types';
+export type { ValueType } from './utils/types';
+export type { OptionType } from './utils/types';
+export type { ValueUpdate } from './utils/types';
+export type { SmartComponentElementProps } from './utils/types';
+export { ChatMessageCreator } from './utils/types';
+export type { ChatMessage } from './utils/types';
+
--- src/src/server.ts
+++ src/src/server.ts
@@ -1,1 +1,2 @@
 export {callAgent} from "./server/index.ts"
+export type { AgentResponse } from './utils/types';
